a36597dadf89ca871c309cc388de25ddaea87b83
Mingsheng Zhang <1046724509@qq.com>
2024-10-28 15:08:26 +0800

    0.1.7

--- Example/zawsdk_ios/main.m
+++ Example/zawsdk_ios/main.m
@@ -12,6 +12,14 @@
 int main(int argc, char * argv[])
 {
     @autoreleasepool {
+        // 获取当前语言环境
+        NSLocale *currentLocale = [NSLocale currentLocale];
+        
+        // 获取语言代码
+        NSString *languageCode = [currentLocale objectForKey:NSLocaleLanguageCode];
+        
+        // 打印语言代码
+        NSLog(@"当前语言的langcode是: %@", languageCode);
         return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZTAppDelegate class]));
     }
 }
